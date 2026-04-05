17c1988184729548b1d0365e4b688ce04797ad91
McRubui <rubuimark@gmail.com>
2026-04-05 19:15:58 +0300

    Update script.js

--- script.js
+++ script.js
@@ -22,7 +22,7 @@ onApprove: function(data, actions) {
 
 return actions.order.capture().then(function(details) {
 
-alert("Transaction completed by " + details.payer.name.given_name);
+alert("Payment completed by " + details.payer.name.given_name);
 
 });
 
