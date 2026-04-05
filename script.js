61a8107669662b3eee04a44f54f0eebc5a9a15de
McRubui <rubuimark@gmail.com>
2026-04-05 14:01:29 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,31 @@
+paypal.Buttons({
+
+createOrder: function(data, actions) {
+
+return actions.order.create({
+
+purchase_units: [{
+
+amount: {
+
+value: '24.00'
+
+}
+
+}]
+
+});
+
+},
+
+onApprove: function(data, actions) {
+
+return actions.order.capture().then(function(details) {
+
+alert("Transaction completed by " + details.payer.name.given_name);
+
+});
+
+}
+
+}).render('#paypal-button-container');
