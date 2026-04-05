29264fefe1b88a0f41ca48ea4179072ef4d27c4a
McRubui <rubuimark@gmail.com>
2026-04-05 19:36:33 +0300

    Create product.js

---
+++ product.js
@@ -0,0 +1,27 @@
+const products = [
+
+{
+id:1,
+name:"Midnight Drive Tee",
+price:24,
+image:"images/shirt1.jpg",
+printify_variant_id:"123456"
+},
+
+{
+id:2,
+name:"Turbo Night Hoodie",
+price:45,
+image:"images/hoodie1.jpg",
+printify_variant_id:"456789"
+},
+
+{
+id:3,
+name:"RM Motorsport Cap",
+price:20,
+image:"images/cap1.jpg",
+printify_variant_id:"987654"
+}
+
+];
